e820e04ab958a949deb35bb0d1047d8251c3691d
Alex Shevchenko <alex.shevchenko@near.org>
2021-01-21 22:32:28 +0200

    Add approve tx, add an untested lock tx

---
+++ src/connector-lock.ts
@@ -0,0 +1,23 @@
+import { ethers, Wallet } from "ethers";
+
+import ethereumConfig from './json/ethereum-config.json';
+import connectorAbi from './json/connector.json';
+import nearConfig from './json/near-config.json';
+
+async function main(){
+  const provider = new ethers.providers.JsonRpcProvider(ethereumConfig.JsonRpc);
+  const signer = new ethers.Wallet(ethereumConfig.PrivateKey, provider);
+  const connector = new ethers.Contract(ethereumConfig.ConnectorAddress, connectorAbi, signer);
+
+  const lockTx = await connector.lockToken(ethereumConfig.TokenAddress, ethers.BigNumber.from(ethereumConfig.TransferAmount), nearConfig.Account);
+  console.log(lockTx);
+}
+
+main().then(
+  text => {
+      console.log(text);
+  },
+  err => {
+      console.log(err);
+  }
+);
---
+++ src/erc20-approve.ts
@@ -0,0 +1,22 @@
+import { ethers, Wallet } from "ethers";
+
+import ethereumConfig from './json/ethereum-config.json';
+import erc20Abi from './json/erc20.json';
+
+async function main() {
+  const provider = new ethers.providers.JsonRpcProvider(ethereumConfig.JsonRpc);
+  const signer = new ethers.Wallet(ethereumConfig.PrivateKey, provider);
+  const erc20 = new ethers.Contract(ethereumConfig.TokenAddress, erc20Abi, signer);
+
+  const approveTx = await erc20.approve(signer.address, ethers.BigNumber.from(ethereumConfig.TransferAmount));
+  console.log(approveTx);
+}
+
+main().then(
+  text => {
+      console.log(text);
+  },
+  err => {
+      console.log(err);
+  }
+);
