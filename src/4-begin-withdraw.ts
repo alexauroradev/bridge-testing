7ed40bc43b7ba56bcfeb48662d43c42e6d815437
Alex Shevchenko <alex.shevchenko@near.org>
2021-02-12 02:44:54 +0200

    Prettify begin withdraw script

--- src/4-begin-withdraw.ts
+++ src/4-begin-withdraw.ts
@@ -1,13 +1,14 @@
 import ethereumConfig from "./json/ethereum-config.json";
 import nearConfig from "./json/near-config.json";
-const nearAPI = require("near-api-js");
 import BN from "bn.js";
 
-const keyStore = new nearAPI.keyStores.UnencryptedFileSystemKeyStore(
-  nearConfig.KeyStore
-);
+// NEAR keystore init
+const nearAPI = require("near-api-js");
+const keyStore = new nearAPI.keyStores.UnencryptedFileSystemKeyStore(nearConfig.KeyStore);
 
 async function main() {
+  console.log("Starting withdrawal of", nearConfig.WithdrawAmount, "bridged tokens", ethereumConfig.TokenAddress);
+  console.log("--------------------------------------------------------------------------------")
   const near = await nearAPI.connect({
     deps: {
       keyStore,
@@ -15,51 +16,53 @@ async function main() {
     nodeUrl: nearConfig.JsonRpc,
     networkId: nearConfig.Network,
   });
-
   const account = await near.account(nearConfig.Account);
-
   const connector = new nearAPI.Contract(account, nearConfig.ConnectorAccount, {
     viewMethods: ["get_bridge_token_account_id"],
     changeMethods: [],
   });
 
-  const bridgeTokenAddress = await connector.get_bridge_token_account_id({
+  const bridgedTokenAddress = await connector.get_bridge_token_account_id({
     address: ethereumConfig.TokenAddress.replace("0x", ""),
   });
-  console.log("Bridged token address: ", bridgeTokenAddress);
+  console.log("Bridged token address is", bridgedTokenAddress);
 
-  const bridgedToken = new nearAPI.Contract(account, bridgeTokenAddress, {
-    viewMethods: ["get_balance"],
-    changeMethods: ["withdraw"],
-  });
+  const bridgedToken = new nearAPI.Contract(account, bridgedTokenAddress,
+    {
+      viewMethods: ["get_balance"],
+      changeMethods: ["withdraw"],
+    }
+  );
   const initialBalance = await bridgedToken.get_balance({
     owner_id: nearConfig.Account,
   });
-  console.log("Balance before the withdraw: ", initialBalance);
+  console.log("Bridged token balance of", nearConfig.Account, "before the withdraw:", initialBalance);
 
   const withdrawTx = await account.functionCall(
-    bridgeTokenAddress,
+    bridgedTokenAddress,
     "withdraw",
     {
       amount: nearConfig.WithdrawAmount,
       recipient: ethereumConfig.Address.replace("0x", ""),
     },
-    new BN("3" + "0".repeat(14)) //maximum gas
+    new BN("3" + "0".repeat(14)) // Maximum gas limit
   );
-
+  
   if (withdrawTx.status.Failure) {
-    console.log("Transaction failed");
+    console.log("Because of some reason transaction was not applied as expected");
+    return;
   } else {
-    console.log("Thansaction succeeded");
+    console.log("Withdraw transaction succeeded:", withdrawTx.transaction.hash);
   }
 
+  // Finding the execution outcome that would be used to proove withdraw on the Ethereum side
   const receiptIds = withdrawTx.transaction_outcome.outcome.receipt_ids;
   const txReceiptId = receiptIds[0];
-
   const successReceiptId = withdrawTx.receipts_outcome.find(
     (r) => r.id === txReceiptId
   ).outcome.status.SuccessReceiptId;
-  console.log("Receipt ID: ", successReceiptId);
+  console.log("The receipt of transaction execution outcome:", successReceiptId);
+  console.log("This receipt should be used in the withdraw finalisation step");
   const txReceiptBlockHash = withdrawTx.receipts_outcome.find(
     (r) => r.id === successReceiptId
   ).block_hash;
@@ -67,22 +70,20 @@ async function main() {
   const receiptBlock = await account.connection.provider.block({
     blockId: txReceiptBlockHash,
   });
-  console.log("Block height:", receiptBlock.header.height);
 
   const finalBalance = await bridgedToken.get_balance({
     owner_id: nearConfig.Account,
   });
-  console.log("Balance after the withdraw: ", finalBalance);
+  console.log("Bridged token balance of", nearConfig.Account, "after the withdraw: ", finalBalance);
   console.log(
-    "Now you need to wait until NEAR Client on Ethereum will get a block with a height higher than ",
+    "Now you need to wait until NEAR Client on Ethereum will get a block with a height higher than",
     receiptBlock.header.height,
-    " plus security period (5 minutes for Ropsten)"
+    "plus challenge period"
   );
 }
 
 main().then(
   (text) => {
-    console.log(text);
   },
   (err) => {
     console.log(err);
